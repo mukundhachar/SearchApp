f50de2596d2246494a54c72178ebf88a61ad0c0a
mukundhachar <36997400+mukundhachar@users.noreply.github.com>
2018-04-27 12:33:26 +0530

    Initial commit

---
+++ Backup/SearchApp/Search.aspx.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace SearchApp
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!this.IsPostBack)
+            {
+            
+               BindGrid();
+            }
+
+        }
+
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            BindGrid();
+        }
+
+
+        private void BindGrid()
+        {
+           
+                string ConnectionString = ConfigurationManager.ConnectionStrings["ApplicationServices"].ConnectionString;
+                DataTable SearchResultsTable = new DataTable();
+                SqlConnection conn = new SqlConnection(ConnectionString);
+                try
+                    {
+                SqlCommand cmd = new SqlCommand("spSearch", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Search", txtEmpName.Text.Trim());
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                adapter.Fill(SearchResultsTable);
+
+                if (SearchResultsTable.Rows.Count > 0)
+                {
+                    Session["SearchResultsTable"] = SearchResultsTable;
+                    gvEmpDetails.DataSource = SearchResultsTable;
+                    gvEmpDetails.DataBind();
+                }
+                else
+                {
+                    DataTable emptyDt = new DataTable();
+                    gvEmpDetails.DataSource = emptyDt;
+                    gvEmpDetails.DataBind();
+                }
+
+                }
+                catch (Exception ex)
+                {
+                          
+                }
+                finally
+                {
+                    if (conn != null)
+                    {
+                        conn.Close();
+                    }
+                }
+        
+                 
+        }
+
+
+        private string GetSortDirection(string column)
+        {
+
+            // By default, set the sort direction to ascending.
+            string sortDirection = "ASC";
+
+            // Retrieve the last column that was sorted.
+            string sortExpression = ViewState["SortExpression"] as string;
+
+            if (sortExpression != null)
+            {
+                // Check if the same column is being sorted.
+                // Otherwise, the default value can be returned.
+                if (sortExpression == column)
+                {
+                    string lastDirection = ViewState["SortDirection"] as string;
+                    if ((lastDirection != null) && (lastDirection == "ASC"))
+                    {
+                        sortDirection = "DESC";
+                    }
+                }
+            }
+
+            // Save new values in ViewState.
+            ViewState["SortDirection"] = sortDirection;
+            ViewState["SortExpression"] = column;
+
+            return sortDirection;
+        }
+
+
+
+
+
+        protected void gvEmpDetails_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            gvEmpDetails.PageIndex = e.NewPageIndex;
+            this.BindGrid();
+        }
+
+        protected void gvEmpDetails_Sorting(object sender, GridViewSortEventArgs e)
+        {
+
+            DataTable dt = Session["SearchResultsTable"] as DataTable;
+
+            if (dt != null)
+            {
+
+              
+                dt.DefaultView.Sort = e.SortExpression + " " + GetSortDirection(e.SortExpression);
+                gvEmpDetails.DataSource = Session["SearchResultsTable"];
+                gvEmpDetails.DataBind();
+            }
+
+          
+        }
+
+        
+
+
+
+
+
+    }
+}
---
+++ SearchApp/Search.aspx
@@ -0,0 +1,82 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true"
+    CodeBehind="Search.aspx.cs" Inherits="SearchApp._Default" %>
+<asp:Content ID="HeaderContent" runat="server" ContentPlaceHolderID="HeadContent">
+</asp:Content>
+<asp:Content ID="BodyContent" runat="server" ContentPlaceHolderID="MainContent">
+    <%--    <h2>
+        Welcome to ASP.NET!
+    </h2>
+    <p>
+        To learn more about ASP.NET visit <a href="http://www.asp.net" title="ASP.NET Website">www.asp.net</a>.
+    </p>
+    <p>
+        You can also find <a href="http://go.microsoft.com/fwlink/?LinkID=152368&amp;clcid=0x409"
+            title="MSDN ASP.NET Docs">documentation on ASP.NET at MSDN</a>.
+    </p>--%>
+
+      <div>  
+
+   <table>
+
+    <tr>
+
+    <td> 
+
+       Search Employee
+
+        </td>
+
+        <td>
+
+        <asp:TextBox ID="txtEmpName" runat="server"></asp:TextBox>
+
+        </td>
+
+        <td> 
+
+        <asp:Button ID="btnSearch" runat="server" Text="Search" onclick="btnSearch_Click" />
+
+        </td>
+
+        
+
+        </tr>
+ 
+
+</table>
+
+<table><tr><td><p><asp:Label ID="lblText" runat="server"></asp:Label>  </p></td></tr></table>
+
+ 
+
+          <asp:SqlDataSource ID="SqlDataSource1" runat="server"></asp:SqlDataSource>
+
+ 
+
+<asp:GridView ID="gvEmpDetails" runat="server"  Width="95%" ShowHeaderWhenEmpty="True" EmptyDataText="No records Found"
+              onpageindexchanging="gvEmpDetails_PageIndexChanging" AllowPaging="True" HeaderStyle-HorizontalAlign="Left"  AllowSorting="true"
+              CellPadding="4" ForeColor="#333333" GridLines="None" 
+              onsorting="gvEmpDetails_Sorting" >
+    <AlternatingRowStyle BackColor="White" />
+    <FooterStyle BackColor="#990000" Font-Bold="True" ForeColor="White" />
+    <HeaderStyle BackColor="#990000" HorizontalAlign="Left" Font-Bold="True" ForeColor="White" />
+    <PagerStyle BackColor="#FFCC66" ForeColor="#333333" HorizontalAlign="Center" />
+    <RowStyle BackColor="#FFFBD6" ForeColor="#333333" />
+    <SelectedRowStyle BackColor="#FFCC66" Font-Bold="True" ForeColor="Navy" />
+    <SortedAscendingCellStyle BackColor="#FDF5AC" />
+    <SortedAscendingHeaderStyle BackColor="#4D0000" />
+    <SortedDescendingCellStyle BackColor="#FCF6C0" />
+    <SortedDescendingHeaderStyle BackColor="#820000" />
+
+    </asp:GridView> 
+
+    </div>
+
+
+
+
+
+
+
+
+</asp:Content>
